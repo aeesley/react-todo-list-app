63e7ffef8372b3b8d30b49e8cc01f6a4f0eae9be
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-16 20:04:15 -0600

    added functionality to store user input into an object

--- todo_list_app/src/App.js
+++ todo_list_app/src/App.js
@@ -4,12 +4,49 @@ import './App.css';
 
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+    if(newItem.text!==""){
+      const newItems=[...this.state.items, newItems];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
   render(){
     return (
       <div className="App">
         <header>
-          <form id="todo-form">
-            <input type="text" placeholder="Enter Text"/>
+          <form id="todo-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter Text"
+            value={this.state.currentItem.text}
+            onChange={this.handleInput}/>
             <button type="submit">Add</button>
           </form>
         </header>
