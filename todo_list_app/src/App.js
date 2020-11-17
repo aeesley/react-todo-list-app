7e058e10ec8c7818ab63137393c05bcd07857ae6
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-16 21:03:03 -0600

    added deleting functionality

--- todo_list_app/src/App.js
+++ todo_list_app/src/App.js
@@ -21,6 +21,7 @@ class App extends React.Component{
     }
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this)
   }
   handleInput(e){
     this.setState({
@@ -45,6 +46,13 @@ class App extends React.Component{
       })
     }
   }
+  deleteItem(key){
+    const filteredItems = this.state.items.filter(item =>
+      item.key!==key);
+      this.setState({
+        items:filteredItems
+    })
+  }
   render(){
     return (
       <div className="App">
@@ -56,7 +64,8 @@ class App extends React.Component{
             <button type="submit">Add</button>
           </form>
         </header>
-        <ListItems items = {this.state.items}></ListItems>
+        <ListItems items = {this.state.items}
+        deleteItem ={this.deleteItem}></ListItems>
       </div>
     );
   }
--- todo_list_app/src/ListItems.js
+++ todo_list_app/src/ListItems.js
@@ -6,10 +6,13 @@ function ListItems(props){
         const items = props.items;
         const listItems = items.map(item =>
             {
-                return <div className="list" key="item.key">
+                return <div className="list" key={item.key}>
                     <p>{item.text}
                     <span>
-                        <FontAwesomeIcon className="faicons" icon='trash'/>
+                        <FontAwesomeIcon className="faicons" 
+                        icon='trash'
+                        onClick={ () => props.deleteItem(item.key)
+                        }/>
                     </span>
                     </p>
 
